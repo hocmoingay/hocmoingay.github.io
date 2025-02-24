74e0f5d0bfbc1d2b5a6d93026bbe9f9aecef4cd0
PhucTH <tv.hoangphuc@gmail.com>
2025-02-24 09:03:17 +0700

    add first books

---
+++ config.js
@@ -0,0 +1,8 @@
+const BASE = '/metadata/';
+const CONFIG = {
+    sitename : 'Tủ sách học mỗi ngày',
+    limit: 12,
+    books: BASE + 'books.json',
+    authors: BASE + 'authors.json',
+    bookData: BASE + 'data.json',
+};
---
+++ start.bat
@@ -0,0 +1,4 @@
+php c:/Users/PhucTH/box/bin/sqlite-to-json metadata.db --reverse "books"
+php c:/Users/PhucTH/box/bin/calibre-image-cover-resize.php .
+start http://localhost:4000
+php -S 127.0.0.1:4000
