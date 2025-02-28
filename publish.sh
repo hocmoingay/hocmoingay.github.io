1b37e5f4b5ea8d4f993053ebb7dc8fa95d538657
PhucTH <tv.hoangphuc@gmail.com>
2025-02-28 10:08:12 +0700

    update

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 php ~/box/bin/sqlite-to-json metadata.db --reverse "books"
 php ~/box/bin/calibre-image-cover-resize.php .
-start http://localhost:4000
-start php -S 127.0.0.1:4000
+
+
+git add .
+git ci -m 'update'
+git push
